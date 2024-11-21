641d7938d6099ad0b9275ec22f68f5cf12603d41
muiddd <muidabdulmuid99@gmail.com>
2024-11-21 10:42:35 +0700

    delete kesalahan

--- Kafe01.java
+++ Kafe01.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Kafe01 {
     public static void Menu(String namaPelanggan, boolean isMember, String kodePromo) {
         System.out.println("Selamat datang, " + namaPelanggan + "!");
@@ -25,7 +26,27 @@ public class Kafe01 {
         }
 
     }
-    public static void main(String[] args) {
-        Menu("Andi", true, "DISKON50");
+
+    public static int hitungTotalHarga(int pilihanMenu, int banyakItem){
+        int [] hargaItems = {15000, 20000, 22000, 12000, 10000, 18000};
+
+        int hargaTotal = hargaItems[pilihanMenu - 1] * banyakItem;
+        return hargaTotal;
     }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        Menu("Andi", true, "DISKON50");
+        
+        System.out.print("\nMasukkan nomor menu yang ingin Anda pesan: ");
+        int pilihanMenu = sc.nextInt();
+        System.out.print("Masukkan jumlah item yang ingin dipesan: ");
+        int banyakItem = sc.nextInt();
+
+        int totalHarga = hitungTotalHarga(pilihanMenu, banyakItem, kodePromo);
+
+        System.out.println("Total harga untuk pesanan Anda: Rp" + totalHarga);
+
+        sc.close();
+    }    
 }
